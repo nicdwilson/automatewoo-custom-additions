e5e0fc08fe542cc09a3a9cf9b4744720a94ee7ae
Nic <3624025+nicdwilson@users.noreply.github.com>
2025-05-16 14:30:47 +1200

    Remove giftcard functions

--- automatewoo-custom-additions.php
+++ automatewoo-custom-additions.php
@@ -51,34 +51,9 @@ class Custom_Additions {
 
 	public function __construct() {
 
-		add_filter( 'automatewoo/data_types/includes', array( $this, 'register_data_types' ));
 		add_filter( 'automatewoo/rules/includes', array( $this, 'register_rules' ) );
 		add_filter( 'automatewoo/variables', array( $this, 'register_variables' ) );
 		add_filter( 'automatewoo/triggers', array( $this, 'register_triggers' ) );
-	}
-
-	/**
-	 * Register data types.
-	 *
-	 * @param array $data_types
-	 *
-	 * @return array
-	 */
-	public function register_data_types( array $data_types ) {
-
-		include_once plugin_dir_path( __FILE__ ) . '/includes/triggers/class-gift-card.php';
-
-		$data_types['gift_card'] = plugin_dir_path( __FILE__ ) . '/includes/data-types/class-gift-card.php';
-
-		wc_get_logger()->debug( 'Vars',
-			array(
-				'source'    => 'automatewoo-giftcards',
-				'data'      => $data_types,
-				'backtrace' => false,
-			)
-		);
-
-		return $data_types;
 	}
 
 
@@ -106,7 +81,6 @@ class Custom_Additions {
 	 */
 	public function register_variables( $vars ) {
 
-		$vars['gift_card']['recipient_email'] = plugin_dir_path( __FILE__ ) . 'includes/variables/gift-card-recipient-email-variable.php';
 		$vars['order']['shipping_total'] = plugin_dir_path( __FILE__ ) . 'includes/variables/order-shipping-total-variable.php';
 		$vars['subscription']['shipping_total'] = plugin_dir_path( __FILE__ ) . 'includes/variables/subscription-shipping-total-variable.php';
 
@@ -120,11 +94,9 @@ class Custom_Additions {
 	function register_triggers( $triggers ) {
 
 		include_once plugin_dir_path( __FILE__ ) . '/includes/triggers/class-subscription-shipping-address-update.php';
-		include_once plugin_dir_path( __FILE__ ) . '/includes/triggers/class-gift-card-created.php';
 
 		// set a unique name for the trigger and then the class name
 		$triggers['Subscription shipping address update'] = 'Subscription_Shipping_Address_Update';
-		$triggers['Gift card created'] = 'Gift_Card_Created';
 
 		return $triggers;
 	}
